0a78f08ac6a0fd25bcd89cada648e41531c984fd
KiranMantha <kiranv.mantha@gmail.com>
2021-04-26 17:13:52 +0530

    updated transformer to get constructor argument types

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 [https://dev.to/phenomnominal/converting-typescript-decorators-into-static-code-1og5](https://dev.to/phenomnominal/converting-typescript-decorators-into-static-code-1og5)
 
 [https://medium.com/@phenomnominal/easier-typescript-tooling-with-tsquery-d74f04f2b29d](https://medium.com/@phenomnominal/easier-typescript-tooling-with-tsquery-d74f04f2b29d)
+
+[https://indepth.dev/posts/1045/having-fun-with-angular-and-typescript-transformers](https://indepth.dev/posts/1045/having-fun-with-angular-and-typescript-transformers)
--- transformers/simple-transformer.ts
+++ transformers/simple-transformer.ts
@@ -1,7 +1,6 @@
-import '@abraham/reflection';
 import * as ts from 'typescript';
 
-function isComponentDecorator(node: ts.Decorator) {
+function hasComponentDecorator(node: ts.Node) {
     // You will probably want something more sophisticated
     // that analyzes the import declarations or possibly uses
     // the type checker in an initial pass of the source files
@@ -9,27 +8,36 @@ function isComponentDecorator(node: ts.Decorator) {
     // decorator is a call expression and if its expression
     // has the text "customElement". This definitely won't work
     // in every scenario and might possibly get false positives.
-    const expr = node.expression;
-    if (!ts.isCallExpression(expr))
-        return false;
-
-    if (!ts.isIdentifier(expr.expression))
-        return false;
-
-    console.log(expr.expression.escapedText);
-    return expr.expression.escapedText === "customElement";
+    return node.decorators && node.decorators.filter(d => d.getFullText().trim().startsWith('@customElement')).length > 0;
 }
 
-const simpleTransformer: ts.TransformerFactory<ts.SourceFile> = context => {
+function hasServiceDecorator(node: ts.Node) {
+    // You will probably want something more sophisticated
+    // that analyzes the import declarations or possibly uses
+    // the type checker in an initial pass of the source files
+    // before transforming. This naively just checks if the
+    // decorator is a call expression and if its expression
+    // has the text "customElement". This definitely won't work
+    // in every scenario and might possibly get false positives.
+    return node.decorators && node.decorators.filter(d => d.getFullText().trim().startsWith('@Service')).length > 0;
+}
+
+const getConstructorMethod = (node: ts.ClassDeclaration) => {
+    const constructorMethod = node.members
+        .filter(node => ts.isConstructorDeclaration(node))
+        .map(node => node as ts.MethodDeclaration)[0];
+    return constructorMethod;
+}
+
+const simpleTransformer: ts.TransformerFactory<ts.SourceFile> = (context: ts.TransformationContext) => {
     return sourceFile => {
         const visitor: ts.Visitor = (node: ts.Node): ts.Node => {
             if (ts.isClassDeclaration(node)) {
-                if (node.decorators && node.decorators.length === 1 && isComponentDecorator(node.decorators[0])) {
-                    console.log(node.members[0]);
-                    let types: Array<any> = Reflect.getMetadata("design:paramtypes", node) || [];
-                    console.log({ types });
+                if (node.decorators && node.decorators.length === 1 && hasComponentDecorator(node)) {
+                    const constructor = getConstructorMethod(node);
+                    const constructorParametersTypes = constructor.parameters.map(param => param.type.getText());
+                    console.log(node.name.escapedText, { constructorParametersTypes });
                 }
-                return node;
             }
             return ts.visitEachChild(node, visitor, context);
         };
